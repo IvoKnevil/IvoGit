577d2752a689076d0f9e7d5c100c300abd391ca2
Ivo Nazlic <78ivonaz@gafe.molndal.se>
2020-11-18 11:35:24 +0100

    Added another excercise

--- IvoGit/Program.cs
+++ IvoGit/Program.cs
@@ -7,6 +7,7 @@ namespace IvoGit
     {
         static void Main(string[] args)
         {
+            /*
             int[] arrayofNumbers = new int[5];
             arrayofNumbers[0] = 13;
             arrayofNumbers[1] = 22;
@@ -29,10 +30,23 @@ namespace IvoGit
                     Console.WriteLine("Not this time");
                 }
             }
-            */
+            
             
 
             Console.WriteLine(arrayofNumbers.Contains(myNr));
+            */
+
+
+            int[] firstOne = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] secondOne = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, };
+
+            Console.WriteLine(string.Join(',', firstOne));
+            Console.WriteLine(string.Join(',', secondOne));
+            Console.WriteLine(string.Join(' ', firstOne.Reverse()));
+
+
+
+
 
         }
     }
