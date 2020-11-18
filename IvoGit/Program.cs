ae3544d174288e621777f47d6ff6109b271bd086
Ivo Nazlic <78ivonaz@gafe.molndal.se>
2020-11-18 11:45:50 +0100

    added guess month code

--- IvoGit/Program.cs
+++ IvoGit/Program.cs
@@ -36,15 +36,45 @@ namespace IvoGit
             Console.WriteLine(arrayofNumbers.Contains(myNr));
             */
 
-
+            /*
             int[] firstOne = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int[] secondOne = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, };
 
             Console.WriteLine(string.Join(',', firstOne));
             Console.WriteLine(string.Join(',', secondOne));
             Console.WriteLine(string.Join(' ', firstOne.Reverse()));
+            */
 
+            string[] stringArray = new string[] 
+            { "January", 
+                "Feb", 
+                "march", 
+                "april", 
+                "maj", 
+                "june", 
+                "july", 
+                "augusti", 
+                "september", 
+                "oktober", 
+                "november", 
+                "december" 
+            };
 
+            string number;
+            int choice;
+
+            do
+            {
+
+                Console.WriteLine("Select a number from 1-12");
+                number = Console.ReadLine();
+                choice = Convert.ToInt32(number);
+                if (choice <12)
+                {
+                    Console.WriteLine(stringArray[choice - 1]);
+                }
+
+            } while (choice <=12);
 
 
 
